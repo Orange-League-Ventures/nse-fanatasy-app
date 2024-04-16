c3cf77213c2b35a73d6b24c99775e293719a5c68
venkateswarlusunke <venkateswarlusunke@onelabventures.com>
2024-04-16 10:23:32 +0530

    chages

--- src/common/CustomText.js
+++ src/common/CustomText.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
-const CustomText = ({ children, style }) => {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+const CustomText = ({ text, style }) => {
+  return <Text style={[styles.text, style]}>{text}</Text>;
 };
 
 const styles = StyleSheet.create({
   text: {
     fontFamily: 'Roboto',
-    fontSize: 90,
+    fontSize: 15,
   },
 });
 
--- src/screens/Play.tsx
+++ src/screens/Play.tsx
@@ -1,67 +1,92 @@
 import React from 'react';
-import { Button, Text, TouchableOpacity, View ,StyleSheet} from 'react-native';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import Button from '../common/Button';
+import CustomText from '../common/CustomText';
 
 const Play = ({route}) => {
-  const { openQuiz, setOpenQuiz } = route.params;
-  // const  setOpenQuiz  = props?.params?.setOpenQuiz;
-    console.log(openQuiz,'openQuizfjfaufbuwef');
-    
-    const handleSubmit=()=>{
-      setOpenQuiz(!openQuiz)
-    }
+  const {openQuiz, setOpenQuiz} = route.params;
+
+  const handleSubmit = () => {
+    setOpenQuiz(!openQuiz);
+  };
   return (
     <View>
-        <View style={styles.container}>
-            <Text style={styles.top}>PLAY</Text>
+      <View style={{padding: 20}}>
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: '#D4D4D4',
+            borderRadius: 12,
+            padding: 10,
+            marginBottom: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {/* <Text style={styles.text}>Game -1 Quiz</Text> */}
+
+          {/* <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Play</Text>
+          </TouchableOpacity> */}
+          <CustomText text="Game -1 Quiz" style={styles.text}/>
+          <Button
+            onPress={() => handleSubmit()}
+            title="Play"
+            style={styles.button}
+            textStyle={styles.buttonText}
+          />
         </View>
-        <View style={{padding:20}}>
-        <View style={{ borderWidth: 1, borderColor: '#D4D4D4', borderRadius: 12, padding: 10, marginBottom: 20,justifyContent:'center',alignItems:'center'}}>
-            <Text style={styles.text}>Game -1 Quiz</Text>
-            <TouchableOpacity style={styles.button}>
-                <Text style={styles.buttonText}>Play</Text>
-            </TouchableOpacity>
+        <View
+          style={{
+            borderWidth: 1,
+            borderColor: '#D4D4D4',
+            borderRadius: 12,
+            padding: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          {/* <Text style={styles.text}>Game -2 Pattern Identifier</Text> */}
+          <CustomText text="Game -2 Pattern Identifier" style={styles.text}/>
+          <Button
+            onPress={() => handleSubmit()}
+            title="Play"
+            style={styles.button}
+            textStyle={styles.buttonText}
+          />
         </View>
-        {/* Second Box */}
-        <View style={{ borderWidth: 1, borderColor: '#D4D4D4', borderRadius: 12, padding: 10 ,justifyContent:'center',alignItems:'center'}}>
-            <Text style={styles.text}>Game -2 Pattern Identifier</Text>
-            <TouchableOpacity style={styles.button} onPress={() => handleSubmit()}>
-                <Text style={styles.buttonText}>Play</Text>
-            </TouchableOpacity>
-        </View>
-        </View>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: '#3A2D7D',
-      width:320,
-      padding: 15,
-      borderRadius: 10,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 18,
-      fontWeight: 'bold',
-    },
-    text:{
-        justifyContent:'center',
-        alignItems: 'center',
-        marginBottom:50
-    },
-    container: {
-        // flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    top: {
-        fontSize: 14,
-        fontWeight: '600',
-        paddingTop:30
-    },
-  });
+  button: {
+    backgroundColor: '#3A2D7D',
+    width: 320,
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  container: {
+    // flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  top: {
+    fontSize: 14,
+    fontWeight: '600',
+    paddingTop: 30,
+  },
+});
 
 export default Play;

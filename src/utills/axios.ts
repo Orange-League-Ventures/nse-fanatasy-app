71fdb952287af47971daa7dc480b339b4977e6a5
prerna <prerna@onelabventures.com>
2024-04-17 11:29:59 +0530

    base url fix

--- src/utills/axios.ts
+++ src/utills/axios.ts
@@ -8,9 +8,8 @@ const baseUrl = REACT_APP_BASE_LOCAL_URL;
 
 console.log({ baseUrl });
 
-
 export const axiosInstance = axios.create({
-  baseURL: `http://10.0.2.2:8000/api/v1`,
+  baseURL: `${baseUrl}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   }

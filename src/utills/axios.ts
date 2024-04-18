78f20d73497d3d6ac173ac310ca7969a5c5038db
Keshav Khandelwal <keshav@onelabventures.com>
2024-04-18 14:52:39 +0530

    update

--- src/utills/axios.ts
+++ src/utills/axios.ts
@@ -1,17 +1,16 @@
 import axios from "axios";
 import { REACT_APP_BASE_LOCAL_URL } from "@env"
 
-// console.log({ REACT_APP_BASE_LOCAL_URL });
+console.log({ REACT_APP_BASE_LOCAL_URL });
 
 
-// const baseUrl = REACT_APP_BASE_LOCAL_URL;
+const baseUrl = REACT_APP_BASE_LOCAL_URL;
 
-// console.log({ baseUrl });
+console.log({ baseUrl });
 
 export const axiosInstance = axios.create({
-  baseURL: `http://192.168.244.144:8000/api/v1`,
+  baseURL: `${baseUrl}/api/v1`,
   headers: {
     'Content-Type': 'application/json',
   }
 });
-    

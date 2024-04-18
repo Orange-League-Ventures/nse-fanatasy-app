1de4ec526a066bcb100169daa8c69ca669636ad8
NileshDeshmukh09 <nileshdeshmukh0908@gmail.com>
2024-04-18 16:33:27 +0530

    axios same as dev

--- src/utills/axios.ts
+++ src/utills/axios.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
-import { REACT_APP_BASE_LOCAL_URL } from "@env"
+import {REACT_APP_BASE_LOCAL_URL} from "@env";
 
-console.log({ REACT_APP_BASE_LOCAL_URL });
+const baseUrl = REACT_APP_BASE_LOCAL_URL;
 
-
-// console.log({ REACT_APP_BASE_LOCAL_URL });
+console.log({baseUrl});
 
 export const axiosInstance = axios.create({
-  baseURL: `http://192.168.0.102:8000/api/v1`,
-  headers: {
-    'Content-Type': 'application/json',
-  }
+  baseURL: `${baseUrl}/api`,
 });

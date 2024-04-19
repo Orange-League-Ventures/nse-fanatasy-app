d649d8619a1ea62abdef20c2cd4feb823557eff8
Akash Kushwaha <akash@turnkey.work>
2024-04-19 18:41:02 +0530

    improvised dicitionary component

--- src/services/dictionaryService.ts
+++ src/services/dictionaryService.ts
@@ -1,32 +1,32 @@
-import { axiosInstance } from "../utills/axios"
+import {axiosInstance} from '../utills/axios';
 
-export const fetchWords = async () => {
-    try {
-        const result = await axiosInstance.get(`/dictionary`)
-        return result
-    } catch (error) {
-        console.log("ERROR IN WORDS FETCHING", error);
-    }
-}
+export const fetchWords = async query => {
+  try {
+    const result = await axiosInstance.get(`/dictionary?query=${query}`);
+    console.log('Result--->', result);
+    return result;
+  } catch (error) {
+    console.log('ERROR IN WORDS FETCHING', error);
+  }
+};
 
 export const fetchWordOfTheDay = async () => {
-    try {
-        const result = await axiosInstance.get(`/dictionary/word-of-the-day`)
-        console.log({result});
-        
-        return result
-    } catch (error) {
-        console.log("ERROR IN WORD OF THE DAY  SERVICE", error);
-    }
-}
+  try {
+    const result = await axiosInstance.get(`/dictionary/word-of-the-day`);
+    console.log('Result', result);
 
-export const fetchDefinition = async (id:string) => {
-    try {
-        const result = await axiosInstance.get(`/dictionary/definition?id=${id}`)
+    return result;
+  } catch (error) {
+    console.log('ERROR IN WORD OF THE DAY  SERVICE', error);
+  }
+};
 
-        return result
-    } catch (error) {
-        console.log("ERROR IN DEFINITION SERVICE", error);
-    }
-}
+export const fetchDefinition = async (id: string) => {
+  try {
+    const result = await axiosInstance.get(`/dictionary/definition?id=${id}`);
 
+    return result;
+  } catch (error) {
+    console.log('ERROR IN DEFINITION SERVICE', error);
+  }
+};
